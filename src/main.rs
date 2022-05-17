8fb8a69233aa4b65de97599823495bc5e5ae20d7
slava <slava-varov3@mail.ru>
2022-05-17 15:38:41 +0300

    shader code moved down a file

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,6 @@ use std::fs;
 use glium::glutin;
 use glium::Surface;
 
-struct Shader {
-	vertex_src: String,
-	fragment_src: String
-}
-
-impl Shader {
-	fn new(vertex_shader_path: &str, fragment_shader_path: &str) -> Self {
-		let vertex_shader_src = fs::read_to_string(vertex_shader_path).expect("Can't read vertex shader file!");
-		let fragment_shader_src = fs::read_to_string(fragment_shader_path).expect("Can't read fragment shader file!");
-
-		let shader = Shader { vertex_src: String::from(vertex_shader_src), fragment_src: String::from(fragment_shader_src) };
-
-		return shader;
-	}
-}
-
 fn main() {
 	let event_loop = glutin::event_loop::EventLoop::new();
 	let wb = glutin::window::WindowBuilder::new()
@@ -69,6 +53,22 @@ fn main() {
 struct Vertex {
     position: [f32; 2],
 	color: [f32; 3]
+}
+
+struct Shader {
+	vertex_src: String,
+	fragment_src: String
+}
+
+impl Shader {
+	fn new(vertex_shader_path: &str, fragment_shader_path: &str) -> Self {
+		let vertex_shader_src = fs::read_to_string(vertex_shader_path).expect("Can't read vertex shader file!");
+		let fragment_shader_src = fs::read_to_string(fragment_shader_path).expect("Can't read fragment shader file!");
+
+		let shader = Shader { vertex_src: String::from(vertex_shader_src), fragment_src: String::from(fragment_shader_src) };
+
+		return shader;
+	}
 }
 
 implement_vertex!(Vertex, position, color);
