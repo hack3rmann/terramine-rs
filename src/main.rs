93ea7da5f1e7dfd988e149099b6e1758800708e7
slava <slava-varov3@mail.ru>
2022-05-18 17:34:18 +0300

    Event loop stop fix

--- src/main.rs
+++ src/main.rs
@@ -39,25 +39,27 @@ fn main() {
 	let mut dt = std::time::Instant::now() - last_time;
 
 	/* Event loop run */
-	event_loop.run(move |ev, _, control_flow| {
+	event_loop.run(move |event, _, control_flow| {
 		/* Event match */
-		match ev {
-			/* Window event */
-			glutin::event::Event::WindowEvent { event, .. } => match event {
-				/* Close event */
-				glutin::event::WindowEvent::CloseRequested => {
-					*control_flow = glutin::event_loop::ControlFlow::Exit;
-					return;
-				},
-				_ => return
-			}, 
-			/* Other events */
-			_ => ()
-		}
+		match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
+        }
 
 		/* Control flow */
-		let next_frame_time = std::time::Instant::now() + std::time::Duration::from_micros(16_666_667);
-		*control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+		let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
 		/* Update time stuff */
 		dt = std::time::Instant::now() - last_time;
