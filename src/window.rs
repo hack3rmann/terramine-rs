ca9bce1d4dc1ffbc938e801102a9c9ef37061338
slava <slava-varov3@mail.ru>
2022-05-24 02:48:35 +0300

    Window icon loading

--- src/window.rs
+++ src/window.rs
@@ -4,6 +4,7 @@
 
 /* Glium stuff */
 use glium::glutin;
+use std::fs;
 
 /* Window struct */
 pub struct Window {
@@ -20,11 +21,34 @@ pub enum Exit {
 impl Window {
 	/// Constructs window.
 	pub fn from(width: i32, height: i32, resizable: bool) -> Self {
+		/* Loading icon from bitmap */
+		let icon = {
+			/* Bytes vector from file */
+			let raw_data = fs::read("src/image/TerramineIcon32p.bmp").unwrap();
+
+			/* Bytemap pointer load from 4 bytes of file */
+			/* This pointer is 4 bytes large and can be found on 10th byte position from file begin */
+			let start_bytes: usize =	(raw_data[13] as usize) << 24 |
+										(raw_data[12] as usize) << 16 |
+										(raw_data[11] as usize) << 8  |
+										(raw_data[10] as usize);
+
+			/* Trim useless information */
+			let raw_data = &raw_data[start_bytes..];
+
+			/* Upload data */
+			let mut data = Vec::with_capacity(raw_data.len());
+			data.extend_from_slice(raw_data);
+			glutin::window::Icon::from_rgba(data, 32, 32).unwrap()
+		};
+
+		/* Build the window */
 		let window_builder = glutin::window::WindowBuilder::new()
 			.with_decorations(true)
 			.with_title("Terramine")
 			.with_resizable(resizable)
-            .with_inner_size(glutin::dpi::LogicalSize::new(width, height));
+            .with_inner_size(glutin::dpi::LogicalSize::new(width, height))
+			.with_window_icon(Some(icon));
 
 		Window {
             window_builder: window_builder,
