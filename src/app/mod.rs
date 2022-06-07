7616596a299b04855448522082a9c28a642ceeb3
slava <slava-varov3@mail.ru>
2022-06-08 00:01:39 +0300

    Optional compiling without console

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -54,7 +54,7 @@ impl App {
 		let texture = Texture::from("src/image/log_bottom.png", &graphics.display).unwrap();
 
 		/* Chunk */
-		let chunk_arr = ChunkArray::new(&graphics, 3, 3, 3);
+		let chunk_arr = ChunkArray::new(&graphics, 7, 1, 7);
 
 		App {
 			chunk_arr,
