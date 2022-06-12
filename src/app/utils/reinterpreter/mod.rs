1236e0bc5cb78dd157e9e04a44ef1d5e41bf9e84
slava <slava-varov3@mail.ru>
2022-06-12 21:41:51 +0300

    Reinterpret tests passed

--- src/app/utils/reinterpreter/mod.rs
+++ src/app/utils/reinterpreter/mod.rs
@@ -209,3 +209,90 @@ impl ReinterpretFromBytes for i128 {
 		}
 	}
 }
+
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn reinterpret_u8() {
+		let before: u8 = 23;
+		let after = u8::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_i8() {
+		let before: i8 = 23;
+		let after = i8::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_u16() {
+		let before: u16 = 13243;
+		let after = u16::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_i16() {
+		let before: i16 = 1442;
+		let after = i16::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_u32() {
+		let before: u32 = 41432;
+		let after = u32::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_i32() {
+		let before: i32 = 2454;
+		let after = i32::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_u64() {
+		let before: u64 = 234;
+		let after = u64::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_i64() {
+		let before: i64 = 5424254;
+		let after = i64::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_u128() {
+		let before: u128 = 23452523453452334;
+		let after = u128::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+
+	#[test]
+	fn reinterpret_i128() {
+		let before: i128 = 243523452345;
+		let after = i128::reinterpret_from_bytes(&before.reinterpret_as_bytes());
+
+		assert_eq!(before, after);
+	}
+}
