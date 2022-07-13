ea01e9c2a2394664ae6653361c5a0dc54bcb2fea
slava <slava-varov3@mail.ru>
2022-07-13 20:08:03 +0300

    Write function

--- src/app/utils/saves/mod.rs
+++ src/app/utils/saves/mod.rs
@@ -1,20 +1,53 @@
 pub mod stack_heap;
 
-use std::marker::PhantomData;
+use std::{marker::PhantomData, collections::HashMap, os::windows::prelude::FileExt};
 
-use super::reinterpreter::Reinterpret;
+use super::reinterpreter::{ReinterpretAsBytes, StaticSize};
+use stack_heap::StackHeap;
 
+pub type Offset = u64;
+
+/// Handle for save files framework.
 pub struct Save<E> {
 	name: String,
-	_phantom_data: PhantomData<E>,
+	file: Option<StackHeap>,
+	offests: HashMap<u64, Offset>,
+	eof: Offset,
+
+	_phantom_data: PhantomData<E>
 }
 
-impl<E: Into<u64>> Save<E> {
+impl<E: Copy + Into<u64>> Save<E> {
+	/// Creates new Save struct.
 	pub fn new(name: &str) -> Self {
-		Self { name: name.to_owned(), _phantom_data: PhantomData }
+		Self {
+			name: name.to_owned(),
+			file: None,
+			offests: HashMap::new(),
+			eof: 0,
+
+			_phantom_data: PhantomData
+		}
 	}
 
-	pub fn write(value: &impl Reinterpret, enumerator: E) -> Self {
-		todo!()
+	/// Creates heap-stack folder.
+	pub fn create(mut self, path: &str) -> Self {
+		self.file = Some(StackHeap::new(path, &self.name));
+		return self
+	}
+
+	/// Writes enum-named value to file.
+	pub fn write<T: ReinterpretAsBytes + StaticSize>(mut self, value: &T, enumerator: E) -> Self {
+		/* Write value to file stack */
+		let bytes = value.reinterpret_as_bytes();
+		self.file.as_ref().unwrap().stack.seek_write(&bytes, self.eof).unwrap();
+
+		/* Saving offset of value */
+		self.offests.insert(enumerator.into(), T::static_size() as Offset).expect("Trying to write same data to another place");
+
+		/* Increment of `end of file` */
+		self.eof += T::static_size() as Offset;
+
+		return self
 	}
 }
