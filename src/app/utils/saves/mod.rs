248fcf02256a6875cb241a383ff7b6957607a406
slava <slava-varov3@mail.ru>
2022-07-13 18:54:52 +0300

    Prototype 3

---
+++ src/app/utils/saves/mod.rs
@@ -0,0 +1,18 @@
+use std::marker::PhantomData;
+
+use super::reinterpreter::Reinterpret;
+
+pub struct Save<E> {
+	name: String,
+	_phantom_data: PhantomData<E>,
+}
+
+impl<E: Into<u64>> Save<E> {
+	pub fn new(name: &str) -> Self {
+		Self { name: name.to_owned(), _phantom_data: PhantomData }
+	}
+
+	pub fn write(value: &impl Reinterpret, enumerator: E) -> Self {
+		todo!()
+	}
+}
--- src/app/utils/saves/prototype2.rs
+++ src/app/utils/saves/prototype2.rs
@@ -3,6 +3,10 @@ enum Type {
 	Dimensions,
 }
 
+impl Into<u64> for Type {
+	fn into(self) -> u64 { self as u64 }
+}
+
 fn save() {
 	let save = Save::new(world_dir("world1"))
 		.write(&Version::new(12, 4, 1, 4), Type::Version)
